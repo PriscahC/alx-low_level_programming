4b3c6967a38f764a7b8be1309615e350fea539bb
PriscahC <opiyopriscah4@gmail.com>
2022-10-16 11:34:59 -0700

    printing size of c types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,9 +8,10 @@
 
 int main(void)
 {
-	printf("size of a char: %d\n", sizeof(char));
-	printf("size of a long int: %d\n", sizeof(long int));
-	printf("sizeof a long long int: %d\n", sizeof(long long int));
-	printf("size of float: %d", sizeof(float));
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of an int: %d byte(s)\n", sizeof(int));
+	printf("size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("sizeof a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("size of float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
