a85d64710781730d048cbeef492535685bff48b4
PriscahC <opiyopriscah4@gmail.com>
2022-10-16 06:25:21 -0700

    printing with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
 
-
-
 /**
- *  *
- *   *  * main - Entry point
- *    *
- *     *   *
- *      *
- *       *    * Return: Always 0 (Success)
- *        *
- *         *     */
+ * main - entry point of the code
+ * description - this program prints using printf
+ * return: - value zero is returned
+ */
 
 int main(void)
-
 {
-
-			printf("with proper grammar, but the outcome is a piece of art,\n");
-
-						return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
