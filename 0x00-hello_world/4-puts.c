9e5646fdb69433a6a91ceb623c798b50ff0a4065
PriscahC <opiyopriscah4@gmail.com>
2022-10-16 05:41:00 -0700

    prints based on Betty Style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
 /**
- *  * main- Serves as the entry point for the program
- *   *
- *    * Return: By default returns zero if no erros otherwise returns non-zero value.
- *     */
+ * main - the main function has no arguments
+ * description - printing an output
+ * puts - prints the words folowed by a new line
+ * return - program ends with value
+ */
+
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
