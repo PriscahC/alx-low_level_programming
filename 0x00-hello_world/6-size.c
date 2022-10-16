94c1882a47b0a13ee946ec80e3b58c8669c774af
PriscahC <opiyopriscah4@gmail.com>
2022-10-16 12:29:46 -0700

    executing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,16 @@
 
 int main(void)
 {
-	printf("size of a char: %lu byte(s)\n", sizeof(char));
-	printf("size of an int: %lu byte(s)\n", sizeof(int));
-	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("size of a float: %lu byte(s)\n", sizeof(float));
+	int a;
+	char b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("size of a char: %d byte(s)\n", sizeof(b));
+	printf("size of an int: %d byte(s)\n", sizeof(a));
+	printf("size of a long int: %d byte(s)\n", sizeof(c));
+	printf("size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("size of a float: %d byte(s)\n", sizeof(f));
 	return (0);
 }
