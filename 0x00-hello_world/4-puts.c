e8242f5b48acb1dabbfe1e7bff031a40c8156c64
PriscahC <opiyopriscah4@gmail.com>
2022-10-16 06:13:47 -0700

    executing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,11 @@
  * main - the main function has no arguments
  * description - printing an output
  * puts - prints the words folowed by a new line
- * return - program ends with value
+ * Return: - program ends with value
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
