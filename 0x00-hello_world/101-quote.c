7006f7aa4c47a61f4abb120e78009a6a70fdf5bf
PriscahC <opiyopriscah4@gmail.com>
2022-10-16 13:10:11 -0700

    alternative to printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - code entry point
+ * description - prints defined words
+ * Return: program returns one
+ */
+
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
