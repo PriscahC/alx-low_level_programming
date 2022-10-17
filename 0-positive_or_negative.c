92732faaad33e51dea57bc19578982f23fbb3685
PriscahC <opiyopriscah4@gmail.com>
2022-10-16 19:20:12 -0700

    positive and negative numbers

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - entry point of code
+ * description - using if statements
+ * Return: zero is returned
+ */
+
+int main(void)
+{
+	int n,
+	strand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	return (0);
