451f31976d3324bef0864c999a1afa158482cf3d
PriscahC <opiyopriscah4@gmail.com>
2022-10-18 02:37:37 -0700

    char err and write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - code entry point
- * description - prints defined words
- * Return: program returns one
+ * description - prints defined words to the standard error
+ * Return: program returns 1(error)
  */
 
 int main(void)
 {
-	write(STDOUT_FILENO,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, err, 59);
 	return (1);
 }
