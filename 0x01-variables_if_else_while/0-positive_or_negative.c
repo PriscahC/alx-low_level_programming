11be75f87d2672c37c9907b03c3cebb428c847fe
Priscila A <105573463+PriscahC@users.noreply.github.com>
2022-10-19 09:27:04 -0700

    defining positive and negative values

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,16 +1,24 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-
+#include <stdio.h>
 /**
- * main - entry point of code
- * description - using if statements
- * Return: zero is returned
+ * main - Entry point
+ *
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int n,
-	strand(time(0));
-	n = rand() - RAND_MAX / 2;
+	int n;
 
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
 	return (0);
+}
