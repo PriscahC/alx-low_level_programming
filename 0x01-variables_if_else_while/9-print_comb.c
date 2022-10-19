8a95992b12e32e1c035387379f2dcbb5ba973dbc
Priscila A <105573463+PriscahC@users.noreply.github.com>
2022-10-19 09:57:01 -0700

    prints combinations of single digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,14 +1,23 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - Prints numbers between 0 to 9 with commas and space
+ *
+ * Return: Always 0 for success
+ */
 int main(void)
 {
-		int n;
+	int digit;
 
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
-					/* your code goes there */
-					return (0);
+	for (digit = 48; digit < 58; digit++)
+	{
+		putchar(digit);
+		if (digit != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
