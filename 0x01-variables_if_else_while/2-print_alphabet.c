4feb9502164d76e50af7422819db1088e2409606
Priscila A <105573463+PriscahC@users.noreply.github.com>
2022-10-19 09:36:34 -0700

    prints lowercase alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,23 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - A program that prints the alphabet in
+ * lowercase followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	        int n;
+	int letter;
 
-		srand(time(0));
-	        n = rand() - RAND_MAX / 2;
-	       
-		/* your code goes there */
-		return (0);
+	/* start from a */
+	letter = 'a';
+
+	while (letter <= 'z')
+	{
+	putchar(letter);
+	letter++;
+	}
+	putchar('\n');
+	return (0);
 }
