1f1e9ac7a3c8bbd8de1b47c64b828980322f0eb1
Priscila A <105573463+PriscahC@users.noreply.github.com>
2022-10-25 13:34:34 -0700

    resets number to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,10 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - entry point of the code
- * reset_to_98 - resets initial value
- * description - pointer with parameter int, updates valu
- * description - resets value to 98
- * @n: pointer
- * Return: void
+ * reset_to_98 - resets value in address referenced by pointer.
+ * @n: pointer.
+ *
+ * Return: void.
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
