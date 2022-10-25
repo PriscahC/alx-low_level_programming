b4bd876560874c1216e12c3b84779516568272a1
PriscahC <opiyopriscah4@gmail.com>
2022-10-25 13:12:58 -0700

    creating files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point of the code
+ * reset_to_98 - resets initial value
+ * description - pointer with parameter int, updates valu
+ * description - resets value to 98
+ * @n: pointer
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
