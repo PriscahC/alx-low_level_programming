a07a491ffa937231a92a8f9cc1d21a6d9d6d1b42
PriscahC <opiyopriscah4@gmail.com>
2022-11-08 02:22:48 -0800

    argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of code
+ * description - prints number of arguments
+ * @argv: contains argument arrays
+ * @argc: contains command count
+ * Return: returns zero
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
