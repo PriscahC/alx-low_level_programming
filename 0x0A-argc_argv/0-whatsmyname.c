ae7250642fc7ff193f00f2a144f8b0d86f9d6441
PriscahC <opiyopriscah4@gmail.com>
2022-11-08 02:09:07 -0800

    betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,13 @@
 
 /**
  * main - entry point of the code
- * Description - parameters in main function
- * @argc - counts number of arguments
- * @argv - contains the command line arguments
- * Return - program returns zero
+ * description - parameters in main function
+ * @argc: counts number of arguments
+ * @argv: contains the command line arguments
+ * Return: program returns zero
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
