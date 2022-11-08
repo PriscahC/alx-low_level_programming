fc4e50dd1dcbeec9f320ad948688f9a846dea503
PriscahC <opiyopriscah4@gmail.com>
2022-11-08 03:52:35 -0800

    adds values

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - code entry point
+ * description - adds argv 
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: returns zero
+ */
+
+int main(int argc, char *argv[])
+{
+	for (i = 0; i < argc; i++)
+	{
+		if 
+	}
